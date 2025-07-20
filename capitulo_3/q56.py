77bf4137a0d9a6790b849d014a952e7b4af45185
Lucaslps10 <134088987+Lucaslps10@users.noreply.github.com>
2025-07-20 00:52:29 +0000

    Código atualizado até a questão 58;

--- capitulo_3/q56.py
+++ capitulo_3/q56.py
@@ -1,1 +1,94 @@
 # Arquivo capitulo_3/q56.py
+
+import random
+
+class Pontuacao:
+    def __init__(self, pontos = 0):
+        self.__pontos = pontos
+
+    def adicionar_pontos(self, valor):
+        self.__pontos += valor
+        print(f"Adicionando {valor} pontos...")
+        
+    def mostrar_pontos(self):
+        return f"Pontos: {self.__pontos}"
+
+class Jogador:
+    total_jogadores = 0  # Atributo de classe
+
+    def __init__(self, nome, energia, pontuacao=None):
+        self.nome = nome
+        self.__energia = energia
+        self.pontuacao = pontuacao if pontuacao else Pontuacao()
+        
+        Jogador.total_jogadores += 1  # Incrementa sempre que um jogador é criado
+
+    @property
+    def energia(self):
+        return self.__energia
+    
+    @energia.setter
+    def energia(self, valor):
+        if valor < 0:
+            self.__energia = 0
+        else:
+            self.__energia = valor
+
+
+            
+    def atacar(self, inimigo):
+        if self.__energia < 10:
+            print("Energia insuficiente para atacar, descanse primeiro...")
+            self.descansar()
+            return 
+        dano = random.randint(5, 20)
+        print(f"Jogador atacou {inimigo.nome} e causou {dano} de dano!")
+
+        self.__energia -= 10
+        print(f"Energia restante: {self.__energia}")
+
+        derrotado = inimigo.tomar_dano(dano)
+        if derrotado:
+            self.pontuacao.adicionar_pontos(10)
+        
+    def descansar(self):
+        if self.__energia + 20 > 100:
+            recuperado = 100 - self.__energia
+        else:
+            recuperado = 20
+        self.__energia += recuperado
+        print(f"Jogador descansou e recuperou {recuperado} de energia. Energia atual: {self.energia}")
+
+    def usar_energia(self, valor):
+        if self.__energia < valor:
+            print("Sem energia suficiente!")
+        else:
+            self.__energia -= valor
+            print(f"Energia usada: {valor} Restante: {self.energia}")
+        
+    def recuperar_energia(self, valor):
+        if self.__energia + valor > 100:
+            recuperado = 100 - self.__energia
+        else:
+            recuperado = valor
+
+        self.__energia += recuperado
+        print(f"Jogador recuperou {recuperado} de energia. Energia atual: {self.__energia}")
+
+    
+
+    
+jogador1 = Jogador("Irmão do jorel", 100)
+print(jogador1.nome)
+print(jogador1.energia)
+print(jogador1.pontuacao.mostrar_pontos())
+
+j2 = Jogador("Lucas", 80)
+j3 = Jogador("Jales
+", 90)
+
+print(f"Total de jogadores criados: {Jogador.total_jogadores}")  # Saída: 3
+
+
+
+
--- capitulo_3/q58.py
+++ capitulo_3/q58.py
@@ -1,1 +1,62 @@
 # Arquivo capitulo_3/q58.py
+
+class Jogo:
+    dificuldade_global = 1  # Atributo de classe (valor padrão)
+
+    def __init__(self, dificuldade=None, turno=1):
+        # Usa a dificuldade_global se nenhuma dificuldade for passada
+        if dificuldade in [1, 2, 3]:
+            self.__dificuldade = dificuldade
+        else:
+            self.__dificuldade = Jogo.dificuldade_global
+        self.turno = turno
+
+    @property
+    def dificuldade(self):
+        return self.__dificuldade
+
+    @dificuldade.setter
+    def dificuldade(self, valor):
+        if valor in [1, 2, 3]:
+            self.__dificuldade = valor
+        else:
+            print("Só há três níveis de dificuldade: 1, 2, 3;")
+
+    @classmethod
+    def definir_dificuldade_global(cls, valor):
+        if valor in [1, 2, 3]:
+            cls.dificuldade_global = valor
+            print(f"Dificuldade global definida para {valor}")
+        else:
+            print("Dificuldade global inválida. Use 1, 2 ou 3.")
+
+    @classmethod
+    def obter_dificuldade_global(cls):
+        return cls.dificuldade_global
+
+    def iniciar(self):
+        print("O jogo começou!")
+
+    def iniciar_combate(self, guerreiro, inimigo):
+        self.iniciar()
+        while guerreiro.energia > 0 and inimigo.vida > 0:
+            print(f"\n--- Turno {self.turno} ---")
+            guerreiro.atacar(inimigo)
+            if inimigo.vida <= 0:
+                print("PersonagemDois venceu a batalha!")
+                break
+
+            inimigo.atacar(guerreiro)
+            if guerreiro.energia <= 0:
+                print("O inimigo venceu a batalha!")
+                break
+
+            self.turno += 1
+
+Jogo.definir_dificuldade_global(3) # Padrão
+
+jogo1 = Jogo()  # Vai usar dificuldade_global (3)
+jogo2 = Jogo(2)  # Vai usar dificuldade própria (2)
+
+print(jogo1.dificuldade)  # Dificuldade: 3
+print(jogo2.dificuldade)  # Dificuldade: 2
